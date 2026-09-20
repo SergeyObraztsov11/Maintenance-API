508a15816a74d584bd8a507b6fa55d63e7484a0e
Sergey <markrut23@mail.ru>
2026-09-20 14:40:49 +0300

    feat: wire equipment routes and error handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
+import equipmentRoutes from "./routes/equipmentRoutes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
+
+app.use(express.json({ limit: "100kb" }));
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
+
+app.use("/api/equipment", equipmentRoutes);
+
+app.use(errorHandler);
 
 export default app;
