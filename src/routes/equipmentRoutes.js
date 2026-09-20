b67e018b842a09d480ddcf93d9ef0a4f90473004
Sergey <markrut23@mail.ru>
2026-09-20 21:03:37 +0300

    feat: add request routes and enhance equipment routes with request handling

--- src/routes/equipmentRoutes.js
+++ src/routes/equipmentRoutes.js
@@ -2,11 +2,13 @@
 
 import { Router } from "express";
 import { equipmentController } from "../controllers/equipmentController.js";
+import { requestController } from "../controllers/requestController.js";
 
 const router = Router();
 
 router.get("/", equipmentController.list);
 router.post("/", equipmentController.create);
+router.get("/:id/requests", requestController.listByEquipment);
 router.get("/:id", equipmentController.getById);
 router.patch("/:id", equipmentController.update);
 router.delete("/:id", equipmentController.remove);
